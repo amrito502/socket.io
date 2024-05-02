a1e97b3ac2d33de680142e94ba3e5dfa32f7bb77
amrito502 <amritobosu7@gmail.com>
2024-05-03 03:43:12 +0600

    simple send message in both user

--- server.js
+++ server.js
@@ -4,11 +4,23 @@ const wss = new ws.Server({port:3030});
 // const ws = new WebSocket("ws://localhost:3030")
 // ws.send("hello server")
 
+
+const sockets = []
 wss.on('connection', (socket) => {
+    sockets.push(socket)
     console.log("New User Connected!");
 
     socket.on("message", (data) => {
         const msg = data.toString();
-        socket.send(msg + " - from server");
+        sockets.forEach(st => {
+            if(st != socket){
+                st.send(msg)
+            }
+           
+        })
     });
+
+    socket.on('close', () => {
+        console.log("User Disconnected!!");
+    })
 });
