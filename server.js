dc4159132cbf593c9155ee453a7ac75f1cdc5853
amrito502 <amritobosu7@gmail.com>
2024-05-03 04:15:22 +0600

    express js server added in web socket

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
+const express = require('express')
+const http = require('http')
+const app = express()
+
+const expressHTTPServer = http.createServer(app)
+
+
 const ws = require('ws');
-const wss = new ws.Server({port:3030});
+const wss = new ws.Server({server: expressHTTPServer});
 
 // const ws = new WebSocket("ws://localhost:3030")
 // ws.send("hello server")
@@ -32,3 +39,7 @@ wss.on('connection', (socket) => {
         console.log("User Disconnected!!");
     })
 });
+
+expressHTTPServer.listen(3030, () => {
+    console.log("Server Running @3030")
+})
